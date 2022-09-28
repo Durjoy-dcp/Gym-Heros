5ffcc404fb4bb0e035ca21e1bac3cce58148712c
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-29 00:44:20 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [m, setM] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then(res => res.json())
+      .then(data => setM(data[0]))
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={m.img} className="App-logo" alt="logo" />
     </div>
   );
 }
