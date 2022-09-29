60b12a256162076963890c5661e6552af4242294
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-29 07:32:33 +0600

    card done

--- src/components/ExerciseContainer/ExcerciseContainer.js
+++ src/components/ExerciseContainer/ExcerciseContainer.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Excercise from '../Excercise/Excercise';
 
 const ExcerciseContainer = () => {
+    const [exercises, SetExercises] = useState([]);
+    useEffect(() => {
+        fetch("products.json")
+            .then(res => res.json())
+            .then(data => SetExercises(data))
+    }, []);
     return (
         <div className='row'>
-            <Excercise></Excercise>
-            <Excercise></Excercise>
-            <Excercise></Excercise>
-            <Excercise></Excercise>
-            <Excercise></Excercise>
-            <Excercise></Excercise>
-            <Excercise></Excercise>
-            <Excercise></Excercise>
-            <Excercise></Excercise>
+            {
+                exercises.map(exercise => <Excercise exercise={exercise} key={exercise.id}></Excercise>)
+            }
+
         </div>
     );
 };
