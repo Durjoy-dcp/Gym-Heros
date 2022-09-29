7b8d622ff81fc87bc589b62e1ac4c9839626a88c
Durjoy-dcp <unknownccxv@gmail.com>
2022-09-29 12:03:59 +0600

    localstorage done

--- src/components/BreakPart/BreakPart.js
+++ src/components/BreakPart/BreakPart.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Time from '../Time/Time';
 import './BreakPart.css';
 
 const BreakPart = ({ time }) => {
+    const [selectedTime, setSelectedTime] = useState(0);
+    const updateTime = (breakTime) => {
+        setSelectedTime(breakTime);
+        localStorage.setItem("breakTime", breakTime);
+    }
+    useEffect(() => {
+        // console.log(selectedTime);
+        const storedtime = localStorage.getItem("breakTime");
+        setSelectedTime(storedtime ? storedtime : 0);
+
+    }, [selectedTime]);
     return (
         <div>
             <h4 className='mt-5  mb-2 px-2'>Add A Break</h4>
@@ -10,32 +21,32 @@ const BreakPart = ({ time }) => {
 
                 <div className="form-check">
                     <input className="form-check-input hid-box" type="radio" name="exampleRadios" id="exampleRadios1" value="10" />
-                    <label className="form-check-label" htmlFor="exampleRadios1">
+                    <label onClick={() => updateTime(10)} className="form-check-label" htmlFor="exampleRadios1">
                         10s
                     </label>
                 </div>
                 <div className="form-check">
                     <input className="form-check-input hid-box" type="radio" name="exampleRadios" id="exampleRadios2" value="20" />
-                    <label className="form-check-label" htmlFor="exampleRadios2">
+                    <label onClick={() => updateTime(20)} className="form-check-label" htmlFor="exampleRadios2">
                         20s
                     </label>
                 </div>
                 <div className="form-check">
                     <input className="form-check-input hid-box" type="radio" name="exampleRadios" id="exampleRadios3" value="30" />
-                    <label className="form-check-label" htmlFor="exampleRadios3">
+                    <label onClick={() => updateTime(30)} className="form-check-label" htmlFor="exampleRadios3">
                         30s
                     </label>
                 </div>
                 <div className="form-check">
                     <input className="form-check-input hid-box" type="radio" name="exampleRadios" id="exampleRadios4" value="40" />
-                    <label className="form-check-label" htmlFor="exampleRadios4">
+                    <label onClick={() => updateTime(40)} className="form-check-label" htmlFor="exampleRadios4">
                         40s
                     </label>
                 </div>
 
             </div>
-            <Time time={time}></Time>
-        </div>
+            <Time time={time} selectedTime={selectedTime}></Time>
+        </div >
     );
 };
 
--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Time = ({ time }) => {
+const Time = ({ time, selectedTime }) => {
     return (
         <div className=' my-4'>
             <div className="p-4 my-2" style={{ "background": "rgb(222, 222, 222)" }}>
                 <h4>Exercise Time : {time}s</h4>
             </div>
             <div className="p-4 my-2" style={{ "background": "rgb(222, 222, 222)" }}>
-                <h4>Break TIme : </h4>
+                <h4>Break TIme :{selectedTime}s </h4>
             </div>
             <div>
                 <button className='btn btn-info w-100 my-3'>Activity Completed</button>
